d228bc348c623781fec27faeeec30cb546b43de1
Gary Grainger <60517109+gary9204@users.noreply.github.com>
2020-02-24 09:59:29 -0500

    Close #9
    Initial app headerbar and menu.

--- src/factsheet/view/ui.py
+++ src/factsheet/view/ui.py
@@ -14,6 +14,26 @@ elements.
 .. data:: UI_DIR
 
    Path to directory that contains user interface definition files.
+
+.. data:: ABOUT_APP
+
+   Glade definition for application About dialog.
+
+.. data:: BACKGROUNE_APP
+
+   Glade definition for application Background dialog.
+
+.. data:: DESIGN_APP
+
+   Glade definition for application Design Notes dialog.
+
+.. data:: HELP_APP
+
+   Glade definition for application Help dialog.
+
+.. data:: Release_APP
+
+   Glade definition for application Release Notes dialog.
 """
 
 
@@ -26,3 +46,16 @@ from gi.repository import Gtk   # type: ignore[import]    # noqa: E402
 CANCEL_GTK = True
 
 UI_DIR = os.path.abspath(os.path.dirname(__file__)) + '/ui/'
+
+# Application/Sheet-level guidance dialogs
+builder_guide_sheet = Gtk.Builder.new_from_file(UI_DIR + 'guide_sheet.ui')
+get_object_guide_sheet = builder_guide_sheet.get_object
+
+ABOUT_APP = get_object_guide_sheet('ui_about_app')
+BACKGROUND_APP = get_object_guide_sheet('ui_background_app')
+DESIGN_APP = get_object_guide_sheet('ui_design_app')
+HELP_APP = get_object_guide_sheet('ui_help_app')
+RELEASE_APP = get_object_guide_sheet('ui_release_app')
+
+del builder_guide_sheet
+del get_object_guide_sheet
