82aa5f0b1c62fa4b6049b82d086073a662554634
Gary Grainger <60517109+gary9204@users.noreply.github.com>
2021-05-20 12:00:36 -0400

    Refresh package and guidance documentation
    Changes are from Issue #162 review of documentation
    above module factsheet.control.pool.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,8 +23,8 @@ copyright = '2020, 2021 Gary Grainger'
 author = 'Gary Grainger'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
-version = '0.2'
+release = '0.5.0'
+version = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
--- src/factsheet/control/__init__.py
+++ src/factsheet/control/__init__.py
@@ -2,6 +2,7 @@
 Defines Factsheet control classes.
 
 :doc:`../guide/devel_notes` explains how application Factsheet is based
-on a Model-View-Controller (MVC) design.  Package ``control`` defines
-classes for control components of the application.
+on a Model-View-Controller (MVC) design.
+Package :mod:`~Factsheet.control` defines classes for control components
+of the application.
 """
