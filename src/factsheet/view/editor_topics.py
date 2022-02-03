79cc4225d1d52a7e002370ba6bb1d9d24a670d19
Gary Grainger <60517109+gary9204@users.noreply.github.com>
2022-02-03 15:42:09 -0500

    Switch collapse/expand from signals to actions
    For #256.

--- src/factsheet/view/editor_topics.py
+++ src/factsheet/view/editor_topics.py
@@ -9,19 +9,25 @@ Types and Type Aliases
     TBD
 """
 import gi   # type: ignore[import]
+import logging
 import typing
 
 from pathlib import Path
 
 import factsheet.bridge_ui as BUI
 import factsheet.control.control_sheet as CSHEET
+import factsheet.spec.base_s as SBASE
 import factsheet.view.ui as UI
 
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gio   # type: ignore[import]    # noqa: E402
+from gi.repository import GLib  # type: ignore[import]    # noqa: E402
 from gi.repository import Gtk   # type: ignore[import]    # noqa: E402
 
 
 UiEditorTopics = typing.Union[Gtk.Frame]
+
+logger = logging.getLogger('Main.VTOPICS')
 
 
 class EditorTopics:
@@ -46,6 +52,7 @@ class EditorTopics:
         path_ui = Path(__file__).with_suffix('.ui')
         get_ui_view = UI.GetUiViewByPath(p_path_ui=path_ui)
         self._ui_view = get_ui_view('ui_editor_topics')
+        self._init_actions()
         self._ui_outline_topics = self._control_sheet.new_view_topics()
         site_topics = get_ui_view('ui_site_topics')
         site_topics.add(self._ui_outline_topics)
@@ -53,19 +60,19 @@ class EditorTopics:
         self._ui_outline_topics.append_column(column_name)
         column_title = self._new_column_title()
         self._ui_outline_topics.append_column(column_title)
-        self._init_buttons_depth(get_ui_view)
+        # self._init_buttons_depth(get_ui_view)
 
-    def _init_buttons_depth(self, p_get_ui_view: UI.GetUiView) -> None:
-        """Initialize buttons to expand and collapse the topics outline.
-
-        :param p_get_ui_view: method to get user interface element.
-        """
-        button_collapse = p_get_ui_view('ui_tool_collapse_outline')
-        _ = button_collapse.connect(
-            'clicked', lambda _button: self._ui_outline_topics.collapse_all())
-        button_expand = p_get_ui_view('ui_tool_expand_outline')
-        _ = button_expand.connect(
-            'clicked', lambda _button: self._ui_outline_topics.expand_all())
+    def _init_actions(self):
+        """Initialize actions for buttons on topics outline toolbar."""
+        actions = Gio.SimpleActionGroup()
+        self._ui_view.insert_action_group('outline_topics', actions)
+        handlers = {'collapse-outline': self.on_change_depth,
+                    'expand-outline': self.on_change_depth,
+                    }
+        actions = self._ui_view.get_action_group('outline_topics')
+        for name, handler in handlers.items():
+            UI.new_action_active(
+                p_group=actions, p_name=name, p_handler=handler)
 
     def _markup_cell_name(
             self, _column: Gtk.TreeViewColumn, p_render: Gtk.CellRenderer,
@@ -126,6 +133,76 @@ class EditorTopics:
         column.set_resizable(True)
         column.set_reorderable(True)
         return column
+
+    def on_change_depth(
+            self, p_action: Gio.SimpleAction, _target: GLib.Variant) -> None:
+        """Expand outline
+
+        :param p_action: user activated this action.
+        :param _target: target of action (unused).
+        """
+        name = p_action.get_name()
+        if 'collapse-outline' == name:
+            self._ui_outline_topics.collapse_all()
+        elif 'expand-outline' == name:
+            self._ui_outline_topics.expand_all()
+        else:
+            logger.warning('Unexpected action: {} ({}.{})'
+                           ''.format(name, self.__class__.__name__,
+                                     self.on_change_depth.__name__))
+
+    def on_new_topic(self, _action: Gio.SimpleAction,
+                     _target: GLib.Variant) -> None:
+        """Specify a new topic and add to topic outline.
+
+        The method queries for the placement of new topic, the template
+        for the topic, and topic contents.  The user may cancel at any
+        of the queries.
+        """
+        # Issue #249 stub
+        spec = SBASE.Base(p_name='Cheese Shop',
+                          p_summary='Please select any cheese in the shop!',
+                          p_title='Cheese Specification')
+        spec()
+        return
+
+        raise NotImplementedError
+        # gtk_model = self._view_topics.gtk_view.get_model()
+        # if len(gtk_model):
+        #     assert self._query_place is not None
+        #     placement = self._query_place()
+        # else:
+        #     placement = QPLACE.Placement(None, QPLACE.Order.CHILD)
+        # if placement is None:
+        #     return
+        #
+        # template = self._query_template()
+        # if template is None:
+        #     return
+        #
+        # topic = template()
+        # if topic is None:
+        #     return
+        #
+        # assert self._control is not None
+        # if placement.order is QPLACE.Order.AFTER:
+        #     index, control = self._control.insert_topic_after(
+        #         topic, placement.anchor)
+        # elif placement.order is QPLACE.Order.BEFORE:
+        #     index, control = self._control.insert_topic_before(
+        #         topic, placement.anchor)
+        # elif placement.order is QPLACE.Order.CHILD:
+        #     index, control = self._control.insert_topic_child(
+        #         topic, placement.anchor)
+        # else:
+        #     raise NotImplementedError
+        #
+        # pane = VTOPIC.FormTopic(pm_control=control)
+        # name_topic = hex(topic.id_topic)
+        # self._scenes_topic.add_scene(pane.gtk_pane, name_topic)
+        # path = gtk_model.get_path(index)
+        # self._view_topics.gtk_view.expand_to_path(path)
+        # self._cursor_topics.select_iter(index)
 
     @property
     def ui_view(self) -> UiEditorTopics:
--- test/factsheet_test/view/test_editor_topics.py
+++ test/factsheet_test/view/test_editor_topics.py
@@ -1,9 +1,12 @@
 """
 Unit etsts for class to display and edit topics outline of a factsheet.
 See :mod:`.editor_topics`.
+
+.. include:: /test/refs_include_pytest.txt
 """
-import pytest  # type: ignore[import]
 import gi   # type: ignore[import]
+import logging
+import pytest  # type: ignore[import]
 
 import factsheet.bridge_ui as BUI
 import factsheet.control.control_sheet as CSHEET
@@ -12,6 +15,8 @@ import factsheet.view.ui as UI
 import factsheet.view.editor_topics as VTOPICS
 
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gio   # type: ignore[import]    # noqa: E402
+from gi.repository import GObject as GO  # type: ignore[import]    # noqa: E402
 from gi.repository import Gtk   # type: ignore[import]    # noqa: E402
 
 
@@ -21,6 +26,7 @@ class TestEditorTopics:
     def test_init(self):
         """Confirm initialization."""
         # Setup
+        NAME_ACTIONS = 'outline_topics'
         control_sheet = CSHEET.ControlSheet(p_path=None)
         N_COLUMNS = 2
         C_NAME = 0
@@ -31,6 +37,8 @@ class TestEditorTopics:
         target = VTOPICS.EditorTopics(p_control_sheet=control_sheet)
         assert target._control_sheet is control_sheet
         assert isinstance(target._ui_view, Gtk.Frame)
+        actions = target._ui_view.get_action_group(NAME_ACTIONS)
+        assert isinstance(actions, Gio.SimpleActionGroup)
         assert target._ui_outline_topics.get_model() is (
             target._control_sheet.new_view_topics._ui_model)
         columns = target._ui_outline_topics.get_columns()
@@ -38,6 +46,23 @@ class TestEditorTopics:
         assert TITLE_C_NAME == columns[C_NAME].get_title()
         assert TITLE_C_TITLE == columns[C_TITLE].get_title()
 
+    @pytest.mark.parametrize('NAME_ACTION', [
+        'collapse-outline',
+        'expand-outline',
+        ])
+    def test_init_actions(self, NAME_ACTION):
+        """Confirm initialization of actions for toolbar buttons."""
+        # Setup
+        control_sheet = CSHEET.ControlSheet(p_path=None)
+        target = VTOPICS.EditorTopics(p_control_sheet=control_sheet)
+        NAME_ACTIONS = 'outline_topics'
+        # Test
+        target._init_actions()
+        actions = target._ui_view.get_action_group(NAME_ACTIONS)
+        assert isinstance(actions, Gio.SimpleActionGroup)
+        assert actions.lookup_action(NAME_ACTION) is not None
+
+    @pytest.mark.skip(reason='transitioning to actions from signals')
     def test_init_buttons_depth(self, monkeypatch):
         """Confirm initialization of expand and contract buttons."""
         # Setup
@@ -67,6 +92,50 @@ class TestEditorTopics:
         target._init_buttons_depth(get_ui_view)
         for id_button, signal in EXPECT.items():
             assert signal == patch_calls.signals[id_button]
+
+    @pytest.mark.skip(reason='pending signal implemenation')
+    @pytest.mark.parametrize(
+        'NAME_SIGNAL, NAME_ATTRIBUTE, ORIGIN, N_DEFAULT', [
+            # ('closed', '_context_name', Gtk.Popover, 0),
+            # ('delete-event', '_window', Gtk.ApplicationWindow, 0),
+            # ('changed', '_cursor_topics', Gtk.TreeSelection, 0),
+            ])
+    def test_init_signals(self, NAME_SIGNAL, NAME_ATTRIBUTE, ORIGIN,
+                          N_DEFAULT, gtk_app_window):
+        """| Confirm initialization.
+        | Case: signal connections
+
+        :param NAME_SIGNAL: name of signal.
+        :param NAME_ATTRIBUTE: name of attribute connected to signal.
+        :param ORIGIN: GTK class of connected attribute.
+        :param N_DEFAULT: number of default handlers
+        :param gtk_app_window: fixture :func:`.gtk_app_window`.
+        """
+        # # Setup
+        # assert False
+        # origin_gtype = GO.type_from_name(GO.type_name(ORIGIN))
+        # signal = GO.signal_lookup(NAME_SIGNAL, origin_gtype)
+        # control = CSHEET.g_control_app.open_factsheet(
+        #     p_path=None, p_time=BUI.TIME_EVENT_CURRENT)
+        # target = VSHEET.ViewSheet(p_control=control)
+        # # Test
+        # # # target._window.set_skip_pager_hint(True)
+        # # # target._window.set_skip_taskbar_hint(True)
+        # target._window.set_transient_for(gtk_app_window)
+        #
+        # attribute = getattr(target, NAME_ATTRIBUTE)
+        # n_handlers = 0
+        # while True:
+        #     id_signal = GO.signal_handler_find(
+        #         attribute, GO.SignalMatchType.ID, signal,
+        #         0, None, None, None)
+        #     if 0 == id_signal:
+        #         break
+        #
+        #     n_handlers += 1
+        #     GO.signal_handler_disconnect(attribute, id_signal)
+        #
+        # assert N_DEFAULT + 1 == n_handlers
 
     @pytest.mark.parametrize('METHOD, I_LINE, EXPECT', [
         ('_markup_cell_name', 0, 'Name 0'),
@@ -160,6 +229,126 @@ class TestEditorTopics:
         assert column.get_resizable()
         assert column.get_reorderable()
 
+    @pytest.mark.parametrize('NAME, COLLAPSE', [
+        ('collapse-outline', True),
+        ('expand-outline', False),
+        ])
+    def test_on_change_depth(self, monkeypatch, NAME, COLLAPSE):
+        """Confirm actions that change depth shown of topics outline.
+
+        :param monkeypatch: built-in fixture `Pytest monkeypatch`_.
+        :param NAME: name of action under test.
+        :param COLLAPSE: whether method should collapse outline.
+        """
+        # Setup
+        class PatchTreeView:
+            def __init__(self):
+                self.called_collapse = False
+                self.called_expand = False
+
+            def collapse_all(self):
+                self.called_collapse = True
+
+            def expand_all(self):
+                self.called_expand = True
+
+        patch = PatchTreeView()
+        monkeypatch.setattr(Gtk.TreeView, 'collapse_all', patch.collapse_all)
+        monkeypatch.setattr(Gtk.TreeView, 'expand_all', patch.expand_all)
+        control_sheet = CSHEET.ControlSheet(p_path=None)
+        target = VTOPICS.EditorTopics(p_control_sheet=control_sheet)
+        actions = target._ui_view.get_action_group('outline_topics')
+        action = actions.lookup_action(NAME)
+        # Test
+        target.on_change_depth(action, None)
+        assert patch.called_collapse is COLLAPSE
+        assert patch.called_expand is not COLLAPSE
+
+    def test_on_change_depth_warn(self, caplog):
+        """Confirm warning for unexpected action.
+
+        :param caplog: built-in fixture `Pytest caplog`_.
+        """
+        # Setup
+        control_sheet = CSHEET.ControlSheet(p_path=None)
+        target = VTOPICS.EditorTopics(p_control_sheet=control_sheet)
+        NAME = 'Oops'
+        action = Gio.SimpleAction.new(NAME, None)
+        N_LOGS = 1
+        LAST = -1
+        log_message = ('Unexpected action: ' + NAME +
+                       ' (EditorTopics.on_change_depth)')
+        # Test
+        target.on_change_depth(action, None)
+        assert N_LOGS == len(caplog.records)
+        record = caplog.records[LAST]
+        assert log_message == record.message
+        assert 'WARNING' == record.levelname
+
+    @pytest.mark.skip(reason='stub method pending implementation')
+    def test_on_new_topic(self, capfd):
+        """| Confirm response to request to specify topic.
+        | Case: user completes placement, template selection, and topic
+          specification.
+
+        :param capfd: built-in fixture `Pytest capfd`_.
+        :param monkeypatch: built-in fixture `Pytest monkeypatch`_.
+        """
+        # # Setup
+        # factsheet = patch_factsheet()
+        # target = VSHEET.ViewSheet(px_app=factsheet)
+        # snapshot = capfd.readouterr()   # Resets the internal buffer
+        # assert not snapshot.out
+        # assert 'Gtk-CRITICAL' in snapshot.err
+        # assert 'GApplication::startup signal' in snapshot.err
+        # # target._window.set_skip_pager_hint(True)
+        # # target._window.set_skip_taskbar_hint(True)
+        # target._window.set_transient_for(WINDOW_ANCHOR)
+        #
+        # sheets_active = CPOOL.PoolSheets()
+        # control = CSHEET.ControlSheet.new(sheets_active)
+        # target.link_factsheet(target, control)
+        # gtk_model = target._view_topics.gtk_view.get_model()
+        # NAME = 'Parrot'
+        # SUMMARY = 'A sketch about customer service.'
+        # TITLE = 'The Parrot Sketch'
+        # topic = MTOPIC.Topic(p_name=NAME, p_summary=SUMMARY, p_title=TITLE)
+        # index_0 = gtk_model.append(None, [topic])
+        #
+        # PATH_EXPECT = '0:0'
+        # placement = QPLACE.Placement(index_0, QPLACE.Order.CHILD)
+        # patch_place = PatchCall(placement)
+        # monkeypatch.setattr(
+        #     QPLACE.QueryPlace, '__call__', patch_place.__call__)
+        #
+        # query_template = target._query_template
+        # outline = query_template._outline
+        # model = outline.gtk_view.get_model()
+        # i_first = model.get_iter_first()
+        # template = AOUTLINE.get_item_gtk(model, i_first)
+        # patch_query_template = PatchCall(template)
+        # monkeypatch.setattr(QTEMPLATE.QueryTemplate, '__call__',
+        #                     patch_query_template.__call__)
+        #
+        # patch_template = PatchCall(topic)
+        # monkeypatch.setattr(
+        #     XSPEC_NOTE.SpecNote, '__call__', patch_template.__call__)
+        # # Test
+        # target.on_new_topic(None, None)
+        # assert patch_place.called
+        # assert patch_query_template.called
+        # assert patch_template.called
+        # id_visible = target._scenes_topic.show_scene(hex(topic.tag))
+        # assert id_visible == hex(topic.tag)
+        # topics, i_new = target._cursor_topics.get_selected()
+        # assert PATH_EXPECT == gtk_model.get_string_from_iter(i_new)
+        # topic_new = AOUTLINE.get_item_gtk(topics, i_new)
+        # assert topic_new is topic
+        # # Teardown
+        # target._window.destroy()
+        # del target._window
+        # del factsheet
+
     @pytest.mark.parametrize('NAME_PROP, NAME_ATTR', [
         ('ui_view', '_ui_view'),
         ])
@@ -198,3 +387,16 @@ class TestModule:
         # Setup
         # Test
         assert TYPE_TARGET == TYPE_EXPECT
+
+    @pytest.mark.parametrize('ATTR, TYPE_EXPECT', [
+        (VTOPICS.logger, logging.Logger),
+        ])
+    def test_attributes(self, ATTR, TYPE_EXPECT):
+        """Confirm type alias definitions.
+
+        :param TYPE_TARGET: type alias under test.
+        :param TYPE_EXPECT: type expected.
+        """
+        # Setup
+        # Test
+        assert isinstance(ATTR, TYPE_EXPECT)
