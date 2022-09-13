ef90a7dbe1f88ed3225eaa0b1eda9f0ff5e6b800
Gary Grainger <60517109+gary9204@users.noreply.github.com>
2022-09-13 10:40:36 -0400

    Add TextAbc facade class
    TextAbc is abstract base class for facade classes
    of text storage elements.
    Plan is to move file consistency checks to
    separate classes.
    
    Close #280.

--- src/factsheet/element_gtk3/model/text/text_abc.py
+++ src/factsheet/element_gtk3/model/text/text_abc.py
@@ -1,3 +1,99 @@
 """
 Define abstract base class for facade classes of text storage elements.
 """
+import typing
+
+import factsheet.element_gtk3.model.model_abc as EMABC
+
+
+class TextAbc(EMABC.ModelGtk3[EMABC.UiModelOpaque, str],
+              typing.Generic[EMABC.UiModelOpaque]):
+    """Common ancestor of facade classes for text storage elements.
+
+    Class extends
+    :class:`.element_gtk3.model.model_abc.ModelGtk3` with property to
+    set and get text stored in memory.
+
+    .. admonition:: Plan
+
+        The current plan is to move consistency checks to control
+        classes.  The goal is to eliminate transient data associated
+        with model components.
+
+        The wisdom of the move is uncertain.  Hence, consistency
+        methods, tests, and documentation are commented out pending the
+        move.::
+
+            Class adds interface
+            :class:`.element_gtk3.model.model_abc.Consistency` to track
+            in-memory text against text in file.
+
+            .. admonition:: About Equality
+
+                Consistency between in-memory text and text in file is a
+                transient state.  Consistency is not saved to file and
+                is not considered when testing for equality.  Class
+                extends methods :meth:`ModelGtk3.__getstate___` and
+                :meth:`.ModelGtk3.__setstate__` to set consistency when
+                class is written to or read from file.
+    """
+
+    # def __getstate__(self) -> typing.Dict:
+    #     """Return text storage element in form pickle can persist.
+    #
+    #     Extends method to remove consistency state.
+    #     """
+    #     state = super().__getstate__()
+    #     # del state['_stale']
+    #     return state
+
+    def __init__(self, p_text: str = '') -> None:
+        """Extend initialization with text and consistency state.
+
+        :param p_text: initial text content.
+        """
+        super().__init__()
+        self.set_internal(p_text)
+        # self._stale = False
+
+    # def __setstate__(self, p_state: typing.MutableMapping) -> None:
+    #     """Extend text bridge reconstruction with change state.
+    #
+    #     Reconstructed text bridge is marked consistent with file.
+    #
+    #     :param p_state: unpickled content.
+    #     """
+    #     super().__setstate__(p_state)
+    #     # self._stale = False
+
+    # def alike(self) -> bool:
+    #     """Return True when there are no unsaved changes to stored text."""
+    #     pass
+    #     # return not self.is_stale()
+
+    # def differ(self) -> bool:
+    #     """Return True when there is at least one unsaved change to
+    #     stored text.
+    #     """
+    #     pass
+    #     # return self._stale
+
+    # def set_alike(self) -> None:
+    #     """Mark text stored in memory consistent with text in file."""
+    #     raise NotImplementedError
+    #     # self._stale = False
+
+    # def set_differ(self) -> None:
+    #     """Mark text stored in memory different from text in file."""
+    #     raise NotImplementedError
+    #     # self._stale = True
+
+    @property
+    def text(self) -> str:
+        """Return model text."""
+        return self.to_external()
+
+    @text.setter
+    def text(self, p_text: str) -> None:
+        """Set model text."""
+        self.set_internal(p_text)
--- test/factsheet_test/element_gtk3/model/text/test_text_abc.py
+++ test/factsheet_test/element_gtk3/model/text/test_text_abc.py
@@ -2,3 +2,189 @@
 Unit tests for text storage element facade base class.  See
 :mod:`.element_gtk3.model.text.text_abc`.
 """
+import pytest
+
+import factsheet.element_gtk3.model.text.text_abc as EMTEXT
+
+
+class PatchText(EMTEXT.TextAbc[str]):
+    """:class:`.element_gtk3.model.text.text_abc.TextAbc` subclass
+    with stubs for abstract methods.
+
+        :param args: patch and superclass positional parameters.
+        :param kwargs: patch and superclass keyword parameters.
+    """
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    def new_ui_model(self):
+        """Stub: return empty string."""
+        # self._ui_model = ''
+        return ''
+
+    def set_internal(self, p_external):
+        """Stub: set model to string."""
+        self._ui_model = str(p_external)
+
+    def to_external(self):
+        """Stub: return model string."""
+        return self._ui_model
+
+
+class TestModelText:
+    """Unit tests for :class:`.element_gtk3.model.text.text_abc.TextAbc`."""
+
+    @pytest.mark.parametrize('CLASS, NAME_METHOD', [
+        (EMTEXT.TextAbc, 'new_ui_model'),
+        (EMTEXT.TextAbc, 'set_internal'),
+        (EMTEXT.TextAbc, 'to_external'),
+        ])
+    def test_method_abstract(self, CLASS, NAME_METHOD):
+        """Confirm each abstract method is specified.
+
+        :param CLASS: class that should be abstract.
+        :param NAME_METHOD: method that should be abstract.
+        """
+        # Setup
+        # Test
+        assert hasattr(CLASS, '__abstractmethods__')
+        assert NAME_METHOD in CLASS.__abstractmethods__
+
+    def test_eq(self):
+        """Confirm equality comparison.
+
+        #. Case: not a text model.
+        #. Case: different text.
+        #. Case: equal
+        """
+        # Setup
+        TEXT = 'The Parrot Sketch'
+        source = PatchText()
+        source.set_internal(TEXT)
+        # Test: not a text attribute.
+        assert not source.__eq__(TEXT)
+        # Test: different content.
+        TEXT_DIFFER = 'Something completely different.'
+        target = PatchText()
+        target.set_internal(TEXT_DIFFER)
+        assert not source.__eq__(target)
+        # Test: equal
+        target = PatchText()
+        target.set_internal(TEXT)
+        assert source.__eq__(target)
+        assert not source.__ne__(target)
+
+    # @pytest.mark.skip
+    # def test_get_set_state(self, tmp_path):
+    #     """Confirm conversion to and from pickle format.
+    #
+    #     :param tmp_path: built-in fixture `Pytest tmp_path`_.
+    #     """
+    #     # Setup
+    #     PATH = Path(str(tmp_path / 'get_set.fsg'))
+    #     source = PatchText()
+    #     TEXT = 'The Parrot Sketch'
+    #     source._set_persist(TEXT)
+    #     source._stale = True
+    #     # Test
+    #     with PATH.open(mode='wb') as io_out:
+    #         pickle.dump(source, io_out)
+    #     with PATH.open(mode='rb') as io_in:
+    #         target = pickle.load(io_in)
+    #     assert source.to_external() == target.to_external()
+    #     assert not target._stale
+
+    def test_init(self):
+        """| Confirm initialization.
+        | Case: non-default text.
+        """
+        # Setup
+        TEXT = 'The Parrot Sketch'
+        # Test
+        target = PatchText(p_text=TEXT)
+        assert hasattr(target, '_ui_model')
+        assert TEXT == target._ui_model
+        # assert isinstance(target._stale, bool)
+        # assert not target._stale
+
+    def test_init_default(self):
+        """| Confirm initialization.
+        | Case: default text.
+        """
+        # Setup
+        BLANK = ''
+        # Test
+        target = PatchText()
+        assert hasattr(target, '_ui_model')
+        assert BLANK == target._ui_model
+        # assert isinstance(target._stale, bool)
+        # assert not target._stale
+
+    def test_str(self):
+        """Confirm return is class name with model text. """
+        # Setup
+        TEXT = 'The Parrot Sketch'
+        target = PatchText()
+        target.set_internal(p_external=TEXT)
+        expect = '<{}: {}>'.format(type(target).__name__, TEXT)
+        # Test
+        assert expect == str(target)
+
+    # @pytest.mark.skip
+    # def test_alike(self):
+    #     """Confirm return matches state. """
+    #     # Setup
+    #     target = PatchText()
+    #     target._stale = False
+    #     # Test
+    #     assert target.is_fresh()
+    #     target._stale = True
+    #     assert not target.is_fresh()
+
+    # @pytest.mark.skip
+    # def test_differ(self):
+    #     """Confirm return matches state. """
+    #     # Setup
+    #     target = PatchText()
+    #     target._stale = False
+    #     # Test
+    #     assert not target.is_stale()
+    #     target._stale = True
+    #     assert target.is_stale()
+
+    # @pytest.mark.skip
+    # def test_set_alike(self):
+    #     """Confirm attribute marked fresh. """
+    #     # Setup
+    #     target = PatchText()
+    #     target._stale = True
+    #     # Test
+    #     target.set_fresh()
+    #     assert not target._stale
+
+    # @pytest.mark.skip
+    # def test_set_differ(self):
+    #     """Confirm attribute marked stale. """
+    #     # Setup
+    #     target = PatchText()
+    #     target._stale = False
+    #     # Test
+    #     target.set_stale()
+    #     assert target._stale
+
+    def test_text(self):
+        """Confirm access limits of text property."""
+        # Setup
+        target_class = PatchText
+        target = target_class()
+        TEXT = 'The Parrot Sketch'
+        target.set_internal(TEXT)
+        TEXT_NEW = 'Something completely different.'
+        # Test
+        assert target_class.text.fget is not None
+        assert TEXT == target.text
+        assert target_class.text.fset is not None
+        target.text = TEXT_NEW
+        assert TEXT_NEW == target.to_external()
+        assert target_class.text.fdel is None
